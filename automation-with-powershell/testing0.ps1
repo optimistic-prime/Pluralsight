7d0112593202a80ddacb66b8009c13fa0d8e0892
Alex Romine <alex.romine@unsupervised.com>
2021-02-20 17:33:54 -0700

    refactor and an initial script add

---
+++ automation-with-powershell/testing0.ps1
@@ -0,0 +1,9 @@
+# $a = 1..10
+# $a.count
+# $a -is [array]
+# $a | Get-Member
+# $a
+# $a[0]
+# $a | ForEach-Object { $_ *2 } | Measure-Object -Sum -Average
+
+Get-Process | Where-Object StartTime | Select-Object Name,Id,@{Name='Run';Expression={(Get-Date)-$_.starttime}} | Sort-Object -Property Run -Descending | Select-Object -First 5
