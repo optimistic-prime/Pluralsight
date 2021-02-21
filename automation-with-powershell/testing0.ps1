c655bb75349427f66d6c2141320fa5c5135445cb
Alex Romine <alex.romine@unsupervised.com>
2021-02-20 17:47:43 -0700

    still going

--- automation-with-powershell/testing0.ps1
+++ automation-with-powershell/testing0.ps1
@@ -6,4 +6,12 @@
 # $a[0]
 # $a | ForEach-Object { $_ *2 } | Measure-Object -Sum -Average
 
-Get-Process | Where-Object StartTime | Select-Object Name,Id,@{Name='Run';Expression={(Get-Date)-$_.starttime}} | Sort-Object -Property Run -Descending | Select-Object -First 5
+# Get-Process | Where-Object StartTime | Select-Object Name,Id,@{Name='Run';Expression={(Get-Date)-$_.starttime}} | Sort-Object -Property Run -Descending | Select-Object -First 5
+
+Get-Process | Where-Object StartTime | foreach {
+    [PSCustomObject]@{
+        Name = $_.Name
+        Id = $_.Id
+        Run = ((Get-Date)-$_.StartTime)
+    } 
+} | Sort-Object -Property Run -Descending | Select-Object -Last 5
